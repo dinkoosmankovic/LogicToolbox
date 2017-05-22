1d196af561161ed13e642209ac80b8d897a6d1a8
Acimovic Aleksandar <acimovic.alek@gmail.com>
2017-05-22 14:07:03 +0200

    ignore test

---
+++ build/Release/test/CMakeFiles/LogicToolbox-lib_test.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/infloop/Documents/LogicToolbox/test/test_hello.cpp" "/home/infloop/Documents/LogicToolbox/build/Release/test/CMakeFiles/LogicToolbox-lib_test.dir/test_hello.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "test"
+  "../../test"
+  "../../src"
+  "ThirdParty/src/googletest/googletest/include"
+  "../../test/../include/ModuleOne"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/infloop/Documents/LogicToolbox/build/Release/src/ModuleOne/CMakeFiles/LogicToolbox-lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
