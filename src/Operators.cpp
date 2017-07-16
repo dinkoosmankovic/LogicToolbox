4b5d723e989e6d237ddced4f5df118d064703f30
Acimovic Aleksandar <acimovic.alek@gmail.com>
2017-07-16 15:01:37 +0200

    Delete build dir

---
+++ src/Operators.cpp
@@ -0,0 +1,5 @@
+//
+// Created by infloop on 7/16/17.
+//
+
+#include "Operators.h"
---
+++ src/Operators.h
@@ -0,0 +1,14 @@
+//
+// Created by infloop on 7/16/17.
+//
+
+#ifndef LOGICTOOLBOX_OPERATORS_H
+#define LOGICTOOLBOX_OPERATORS_H
+
+
+bool And(const bool x, const bool y){
+    return x&&y;
+}
+
+
+#endif //LOGICTOOLBOX_OPERATORS_H
